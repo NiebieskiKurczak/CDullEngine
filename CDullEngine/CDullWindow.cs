59296994f94cfd1e40ee0afc7af43e0addd74ff0
NB <88508428+NiebieskiKurczak@users.noreply.github.com>
2024-05-25 20:37:16 +0200

    first commit

---
+++ CDullEngine/CDullWindow.cs
@@ -0,0 +1,10 @@
+﻿namespace CDullEngine;
+
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+public class CDullWindow : GameWindow {
+    public CDullWindow(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { ClientSize = (width, height), Title = title }) { }
+}
