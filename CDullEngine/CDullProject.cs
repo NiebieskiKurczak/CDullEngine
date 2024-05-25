4e07ddaf8e53226b12107c8c4bc67a84840839d0
NB <88508428+NiebieskiKurczak@users.noreply.github.com>
2024-05-25 23:44:00 +0200

    Added basic update and start loops

---
+++ CDullEngine/CDullProject.cs
@@ -0,0 +1,12 @@
+namespace CDullEngine;
+
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+public abstract class CDullProject {
+    public virtual void Start() {}
+
+    public virtual void Update() {}
+}
--- CDullEngine/CDullWindow.cs
+++ CDullEngine/CDullWindow.cs
@@ -6,5 +6,19 @@ using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 
 public class CDullWindow : GameWindow {
-    public CDullWindow(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { ClientSize = (width, height), Title = title }) { }
+    public CDullProject cDullProject;
+
+    public CDullWindow(int width, int height, string title, CDullProject cDullProject) : base(GameWindowSettings.Default, new NativeWindowSettings() { ClientSize = (width, height), Title = title }) {
+        this.cDullProject = cDullProject;
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs args) {
+        base.OnUpdateFrame(args);
+        cDullProject.Update();
+    }
+
+    protected override void OnLoad() {
+        base.OnLoad();
+        cDullProject.Start();
+    }
 }
